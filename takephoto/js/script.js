bafe2ae07526c11603a27ec0fad95600fc124bd8
Kim Taehoon <inlove8307@gmail.com>
2017-08-09 22:48:04 +0900

    modify takephoto

--- takephoto/js/script.js
+++ takephoto/js/script.js
@@ -64,6 +64,27 @@ this.Game = this.Game || {};
         'game-load': { 'width': 800, 'height': 2781, 'top': 0, 'left': 0 },
         'game-load-logo': {'width': 400, 'height': 400, 'top': 400, 'left': 200 },
         'game-layer': { 'width': 800, 'height': 2781, 'top': 0, 'left': 0 },
+        'game-select': { 'width': 776, 'height': 1328, 'top': 0, 'left': 12 },
+        'game-select-popcorn': { 'width': 296, 'height': 202, 'top': 285, 'left': 84 },
+        'game-select-puzzle': { 'width': 296, 'height': 202, 'top': 285, 'left': 396 },
+        'game-select-list-popcorn': { 'width': 606, 'height': 534, 'top': 532, 'left': 85 },
+        'game-select-list-puzzle': { 'width': 606, 'height': 534, 'top': 532, 'left': 85 },
+        'game-select-popcorn-muzi': { 'width': 142, 'height': 254, 'top': 0, 'left': 0 },
+        'game-select-popcorn-apeach': { 'width': 142, 'height': 254, 'top': 0, 'left': 155 },
+        'game-select-popcorn-frodo': { 'width': 142, 'height': 254, 'top': 0, 'left': 310 },
+        'game-select-popcorn-neo': { 'width': 142, 'height': 254, 'top': 0, 'left': 464 },
+        'game-select-popcorn-tube': { 'width': 142, 'height': 254, 'top': 280, 'left': 0 },
+        'game-select-popcorn-jayg': { 'width': 142, 'height': 254, 'top': 280, 'left': 155 },
+        'game-select-popcorn-ryan': { 'width': 142, 'height': 254, 'top': 280, 'left': 310 },
+        'game-select-popcorn-con': { 'width': 142, 'height': 254, 'top': 280, 'left': 464 },
+        'game-select-puzzle-muzi': { 'width': 142, 'height': 254, 'top': 0, 'left': 0 },
+        'game-select-puzzle-apeach': { 'width': 142, 'height': 254, 'top': 0, 'left': 155 },
+        'game-select-puzzle-frodo': { 'width': 142, 'height': 254, 'top': 0, 'left': 310 },
+        'game-select-puzzle-neo': { 'width': 142, 'height': 254, 'top': 0, 'left': 464 },
+        'game-select-puzzle-tube': { 'width': 142, 'height': 254, 'top': 280, 'left': 0 },
+        'game-select-puzzle-jayg': { 'width': 142, 'height': 254, 'top': 280, 'left': 155 },
+        'game-select-puzzle-ryan': { 'width': 142, 'height': 254, 'top': 280, 'left': 310 },
+        'game-button-select': { 'width': 349, 'height': 90, 'top': 1125, 'left': 212 },
         'game-success': { 'width': 776, 'height': 514, 'top': 700, 'left': 12 },
         'game-fail': { 'width': 776, 'height': 514, 'top': 700, 'left': 12 },
         'game-button-success-close': { 'width': 349, 'height': 90, 'top': 300, 'left': 224 },
@@ -275,6 +296,7 @@ this.Game = this.Game || {};
     // PROPERTIES
     p.config = null;
     p.loaded = false;
+    p.select = null;
     p.interval = null;
     p.view = null;
     p.item = null;
@@ -288,10 +310,6 @@ this.Game = this.Game || {};
         this.view = new Game.View(800);
         this.view.setResizeAll();
 
-        this.item = new Game.Item(this.config.code, this.config.clone);
-        this.createItem(this.item.getItems());
-        this.createStatus(this.item.getStatus(this.config.code));
-
         this.effect = new Howl({
             src: ['assets/audio/effect.mp3']
         });
@@ -300,6 +318,11 @@ this.Game = this.Game || {};
         this.setIntervalLoad();
 
         this.dev = true;
+    };
+    p.createAll = function(){
+        this.item = new Game.Item(this.config.code, this.config.clone);
+        this.createItem(this.item.getItems());
+        this.createStatus(this.item.getStatus(this.config.code));
     };
     p.createItem = function(items){
         var list, item, style, i, length;
@@ -331,11 +354,11 @@ this.Game = this.Game || {};
         for (i = 0, length = items.length; i < length; i++) {
             item = document.createElement('span');
 
-            if (this.config.success.indexOf(items[i]['code']) === -1) {
-                item.setAttribute('class', items[i]['status-class']);
+            if (this.config.success[items[i]['code']]) {
+                item.setAttribute('class', items[i]['status-class'] + ' on');
             }
             else {
-                item.setAttribute('class', items[i]['status-class'] + ' on');
+                item.setAttribute('class', items[i]['status-class']);
             }
 
             list.appendChild(item);
@@ -369,11 +392,15 @@ this.Game = this.Game || {};
 
         $effect.fadeIn(200, function(){
             $(this).fadeOut(100, function(){
-                setTimeout(function(){
-                    me.showLayerPopup($list, $focus);
-                }, 1000);
+                me.showLayerPopup($list, $focus);
             });
         });
+    };
+    p.showLayerSelect = function(){
+        $(this.view.getItem('game-layer')).fadeIn();
+        $(this.view.getItem('game-select')).fadeIn();
+
+        this.select = 'C02';
     };
     p.showLayerPopup = function($list, $focus){
         var isSuccess;
@@ -381,13 +408,13 @@ this.Game = this.Game || {};
         isSuccess = this.checkSuccess(this.getSelectCode($list, $focus));
 
         if (isSuccess) {
-            this.view.getItem('game-layer').style.display = 'block';
-            this.view.getItem('game-success').style.display = 'block';
+            $(this.view.getItem('game-layer')).fadeIn();
+            $(this.view.getItem('game-success')).fadeIn();
             this.createStatus(this.item.getStatus(this.config.code));
         }
         else {
-            this.view.getItem('game-layer').style.display = 'block';
-            this.view.getItem('game-fail').style.display = 'block';
+            $(this.view.getItem('game-layer')).fadeIn();
+            $(this.view.getItem('game-fail')).fadeIn();
         }
     };
     p.getSelectCode = function($list, $focus){
@@ -417,23 +444,55 @@ this.Game = this.Game || {};
         return result;
     };
     p.checkSuccess = function(code){
-        var check;
-
-        check = [this.config.code, 'LE', 'LP', 'LM'];
-
         // 초점이 맞지 않았을 경우
         if (!code) return false;
         // 선택한 캐릭터 또는 로고가 아닌 경우
-        if (check.indexOf(code) < 0) return false;
+        if (this.config.success[code] === undefined) return false;
         // 이미 성공한 캐릭터 또는 로고일 경우
-        if (this.config.success.indexOf(code) >= 0) return false;
+        if (this.config.success[code]) return false;
 
-        this.config.success.push(code);
+        this.config.success[code] = true;
 
         return true;
     };
     p.setIntervalLoad = function(){
         this.interval = setInterval(this.eventHandlerInterval.bind(this), 3000);
+    };
+    p.eventHandlerSelectPopcorn = function(event){
+        $(this.view.getItem('game-select-popcorn')).addClass('on');
+        $(this.view.getItem('game-select-list-popcorn')).addClass('on');
+        $(this.view.getItem('game-select-popcorn-muzi')).addClass('on');
+
+        this.select = 'C02';
+
+        $(this.view.getItem('game-select-puzzle')).removeClass('on');
+        $(this.view.getItem('game-select-list-puzzle')).removeClass('on');
+        $(this.view.getItem('game-select-list-puzzle')).children().removeClass('on');
+    };
+    p.eventHandlerSelectPuzzle = function(event){
+        $(this.view.getItem('game-select-puzzle')).addClass('on');
+        $(this.view.getItem('game-select-list-puzzle')).addClass('on');
+        $(this.view.getItem('game-select-puzzle-muzi')).addClass('on');
+
+        this.select = 'C10';
+
+        $(this.view.getItem('game-select-popcorn')).removeClass('on');
+        $(this.view.getItem('game-select-list-popcorn')).removeClass('on');
+        $(this.view.getItem('game-select-list-popcorn')).children().removeClass('on');
+    };
+    p.eventHandlerSelectCharacter = function(event){
+        $(event.target).addClass('on').siblings().removeClass('on');
+
+        this.select = $(event.target).data('code');
+    };
+    p.eventHandlerSelectComplete = function(event){
+        this.config.code = this.select;
+        this.config.success[this.config.code] = false;
+
+        this.createAll();
+
+        $(this.view.getItem('game-layer')).fadeOut();
+        $(this.view.getItem('game-select')).fadeOut();
     };
     p.eventHandlerPlay = function(event){
         if (this.dev) {
@@ -453,14 +512,21 @@ this.Game = this.Game || {};
         if (this.dev) this.view.getItem('game-button-play').style.display = 'inline-block';
     };
     p.eventHandlerClose = function(){
-        this.view.getItem('game-layer').style.display = 'none';
-        this.view.getItem('game-success').style.display = 'none';
-        this.view.getItem('game-fail').style.display = 'none';
+        $(this.view.getItem('game-layer')).fadeOut();
+        $(this.view.getItem('game-success')).fadeOut();
+        $(this.view.getItem('game-fail')).fadeOut();
     };
     p.eventHandlerInterval = function(){
         if (this.loaded) {
             $(this.view.getItem('game-load')).fadeOut();
             clearInterval(this.interval);
+
+            if (this.config.code) {
+                this.createAll();
+            }
+            else {
+                this.showLayerSelect();
+            }
         }
     };
     p.eventHandlerLoad = function(event){
@@ -471,6 +537,24 @@ this.Game = this.Game || {};
         this.view.getItem('game-button-stop').addEventListener('touchstart', this.eventHandlerStop.bind(this));
         this.view.getItem('game-button-success-close').addEventListener('touchstart', this.eventHandlerClose.bind(this));
         this.view.getItem('game-button-fail-close').addEventListener('touchstart', this.eventHandlerClose.bind(this));
+        this.view.getItem('game-select-popcorn').addEventListener('touchstart', this.eventHandlerSelectPopcorn.bind(this));
+        this.view.getItem('game-select-puzzle').addEventListener('touchstart', this.eventHandlerSelectPuzzle.bind(this));
+        this.view.getItem('game-select-popcorn-muzi').addEventListener('touchstart', this.eventHandlerSelectCharacter.bind(this));
+        this.view.getItem('game-select-popcorn-neo').addEventListener('touchstart', this.eventHandlerSelectCharacter.bind(this));
+        this.view.getItem('game-select-popcorn-frodo').addEventListener('touchstart', this.eventHandlerSelectCharacter.bind(this));
+        this.view.getItem('game-select-popcorn-jayg').addEventListener('touchstart', this.eventHandlerSelectCharacter.bind(this));
+        this.view.getItem('game-select-popcorn-ryan').addEventListener('touchstart', this.eventHandlerSelectCharacter.bind(this));
+        this.view.getItem('game-select-popcorn-con').addEventListener('touchstart', this.eventHandlerSelectCharacter.bind(this));
+        this.view.getItem('game-select-popcorn-apeach').addEventListener('touchstart', this.eventHandlerSelectCharacter.bind(this));
+        this.view.getItem('game-select-popcorn-tube').addEventListener('touchstart', this.eventHandlerSelectCharacter.bind(this));
+        this.view.getItem('game-select-puzzle-muzi').addEventListener('touchstart', this.eventHandlerSelectCharacter.bind(this));
+        this.view.getItem('game-select-puzzle-neo').addEventListener('touchstart', this.eventHandlerSelectCharacter.bind(this));
+        this.view.getItem('game-select-puzzle-frodo').addEventListener('touchstart', this.eventHandlerSelectCharacter.bind(this));
+        this.view.getItem('game-select-puzzle-jayg').addEventListener('touchstart', this.eventHandlerSelectCharacter.bind(this));
+        this.view.getItem('game-select-puzzle-ryan').addEventListener('touchstart', this.eventHandlerSelectCharacter.bind(this));
+        this.view.getItem('game-select-puzzle-apeach').addEventListener('touchstart', this.eventHandlerSelectCharacter.bind(this));
+        this.view.getItem('game-select-puzzle-tube').addEventListener('touchstart', this.eventHandlerSelectCharacter.bind(this));
+        this.view.getItem('game-button-select').addEventListener('touchstart', this.eventHandlerSelectComplete.bind(this));
         window.addEventListener('load', this.eventHandlerLoad.bind(this));
     };
 
@@ -481,14 +565,49 @@ this.Game = this.Game || {};
 (function(window, document, $, undefined){
     "use strict";
 
-    var play = new Game.Play({
-        'isToday': false, // 이벤트 참여 여부 (set response data)
-        'code': 'C07', // 선택 캐릭터 코드 (set response data)
+    var response, options;
+
+    response = {
+        'inToday': 'N',
+        'M3C': 'N',
+        'LE': 'N',
+        'LP': 'N',
+        'LM': 'N',
+        'missionClear3': 'N'
+    };
+
+    options = {
+        'code': false, // 선택 캐릭터 코드 (set response data)
+        'inToday': false, // 당일 미션 참가 여부 (set response data)
+        'missionClear3': false, // 미션 완료 여부 (set response data)
+        'success': {
+            'LE': false, // 이마트 로고 성공 여부 (set response data)
+            'LP': false, // 프렌즈팝콘 로고 성공 여부 (set response data)
+            'LM': false // 프렌즈사천성 로고 성공 여부 (set response data)
+        },
         'clone': 1, // 선택 캐릭터 중복 출현 빈도
         'speed': 300, // 캐릭터 슬라이드 속도
-        'ratio': 0.3, // 캐릭터 영역 내부 체크 비율 (0.0 ~ 1.0)
-        'success': [] // 성공한 아이템 코드 (set response data)
-    });
+        'ratio': 0.3 // 캐릭터 체크 비율 (0.0 ~ 1.0)
+    };
+
+    // 캐릭터 선택 여부 (code or 'N')
+    if (response['M3C'] !== 'N') options['code'] = response['M3C'];
+    // 당일 게임 참가 여부 ('Y' or 'N')
+    if (response['inToday'] === 'Y') options['inToday'] = true;
+    // 미션 완료 여부 ('Y' or 'N')
+    if (response['missionClear3'] === 'Y') options['missionClear3'] = true;
+    // 캐릭터 선택한 경우 SUCCESS에 코드 추가
+    if (options['code']) options.success[options['code']] = false;
+    // 선택 캐릭터 성공 여부 ('Y' or 'N')
+    if (response[options['code']] === 'Y') options.success[options['code']] = true;
+    // 이마트 로고 성공 여부
+    if (response['LE'] === 'Y') options.success['LE'] = true;
+    // 프렌즈팝콘 로고 성공 여부
+    if (response['LP'] === 'Y') options.success['LP'] = true;
+    // 프렌즈사천성 로고 성공 여부
+    if (response['LM'] === 'Y') options.success['LM'] = true;
+
+    Game.today = new Game.Play(options);
 
     // var console = (function(oldConsole){
     //     var elConsole, elMessage, elButton, counter = 0;
