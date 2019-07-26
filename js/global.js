a4ae23a7cd78bcf398f60b86f4e7fc53191250f0
Kim Taehoon <inlove8307@gmail.com>
2019-07-26 08:48:59 +0000

    root parallax test

--- js/global.js
+++ js/global.js
@@ -24,13 +24,13 @@ var ROOT = ROOT || {};
   'use strict';
 
   var console = (function(console){
-    var $wrap = $('<section>', { id: 'DEBUG', class: 'debug' })
+    var $wrap = $('<section>', { class: 'debug' })
       , $inner = $('<article>', { class: 'debug-inner' })
       , $title = $('<h2>', { class: 'debug-title', text: 'DEBUG' })
       , $text = $('<div>', { class: 'debug-text' })
       , $button = $('<button>', { class: 'debug-button', text: 'D' })
       , $clear = $('<button>', { class: 'debug-clear', text: 'CLEAR' })
-      , $style = $('<style>', { id: 'DEBUG_CSS' })
+      , $style = $('<style>')
       , style = '\
       .debug{position:fixed;bottom:0;left:0;z-index:9999;width:100%;height:50vh;}\
       .debug-inner{display:flex;flex-flow:column;position:relative;width:100%;height:100%;border-top:1px solid rgba(0, 0, 0, 1);background-color:rgba(255, 255, 255, 0.95);}\
@@ -43,11 +43,11 @@ var ROOT = ROOT || {};
       .debug-button{position:fixed;bottom:2.77vw;right:2.77vw;z-index:9999;margin:0;padding:0;width:6.94vw;height:6.94vw;border:1px solid rgba(255, 255, 255, 1);border-radius:100%;background-color:rgba(0, 0, 0, 1);font-weight:700;font-family:\'Noto Sans KR\';font-size:2.77vw;color:rgba(255, 255, 255, 1);}\
       ';
 
-    $wrap.append($inner, $button, $clear);
+    $wrap.append($inner, $clear);
     $inner.append($title, $text);
 
     $(window).on('load', function(event){
-      $('body').append($wrap, $style.text(style));
+      $('body').append($wrap, $button, $style.text(style));
 
       $button.on('touchstart', function(event){
         $wrap.height()
@@ -76,6 +76,7 @@ var ROOT = ROOT || {};
       delete: function(){
         $(window).on('load', function(event){
           $wrap.remove();
+          $button.remove();
           $style.remove();
         });
       }
